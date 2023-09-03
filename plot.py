ac96cabd6a953a70f6255a664b82888a278ca0ff
wierzba100 <filipwierzbinka@gmail.com>
2023-09-03 17:40:08 +0200

    Plot variable names update

--- plot.py
+++ plot.py
@@ -9,8 +9,8 @@ data = np.load('samples.npy')
 
 channel_1 = []
 channel_2 = []
-x_1 = []
-x_2 = []
+time_1 = []
+time_2 = []
 
 for i, item in enumerate(data):
     if i % 2 == 0:
@@ -19,20 +19,18 @@ for i, item in enumerate(data):
         channel_2.append(int.from_bytes(item, byteorder='big') * ADC_CONVERT)
 
 for i in range(100000):
-    x_1.append(i * 4)
-    x_2.append(2 + i * 4)
-
-print(channel_1)
+    time_1.append(i * 4)
+    time_2.append(2 + i * 4)
 
 plt.subplot(2, 1, 1)
-plt.plot(x_1, channel_1, label='100kHz', color='blue')
+plt.plot(time_1, channel_1, label='100kHz', color='blue')
 plt.title("Channel 1, 250kS/s")
 plt.xlabel("Microseconds") 
 plt.ylabel("Volts")
 plt.legend()
 
 plt.subplot(2, 1, 2)
-plt.plot(x_2, channel_2, label='50kHz', color='red')
+plt.plot(time_2, channel_2, label='50kHz', color='red')
 plt.title("Channel 2, 250kS/s")
 plt.xlabel("Microseconds") 
 plt.ylabel("Volts")
